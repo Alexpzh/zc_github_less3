8bcf8fb5af398fdfdaf268e9cb2270f0e7a6f169
Alexpzh <166602027+Alexpzh@users.noreply.github.com>
2024-06-16 15:38:29 +0300

    Add move

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 pygame.init()
 
+FPS = 30
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
@@ -19,7 +20,8 @@ target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-
+vx = 5
+vy = 5
 running = True
 while running:
     screen.fill(color)
@@ -31,6 +33,24 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                vx = random.randint(-5, 5)
+                vy = random.randint(-5, 5)
+
+    target_x += vx * 0.05
+    target_y += vy * 0.05
+    if target_x < 0:
+        target_x = 0
+        vx =-vx
+    if target_x > SCREEN_WIDTH - target_width:
+        target_x = SCREEN_WIDTH - target_width
+        vx = -vx
+    if target_y < 0:
+        target_y = 0
+        vy = -vy
+    if target_y > SCREEN_HEIGHT - target_height:
+        target_y = SCREEN_HEIGHT - target_height
+        vy = -vy
+
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
